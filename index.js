b35e244b3b008c85de05af2c8ba8dbe7fea3a9f9
brandonpage <bpage1@salesforce.com>
2018-06-29 13:44:44 -0700

    rename

--- index.js
+++ index.js
@@ -33,4 +33,4 @@ import { YellowBox } from 'react-native';
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 
 
-AppRegistry.registerComponent('SmartPerformance', () => App);
+AppRegistry.registerComponent('SmartStorePerformance', () => App);
